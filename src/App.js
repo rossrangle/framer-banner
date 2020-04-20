bd9a2a1761e28f759e43b9f2b75dc71d234a5ad8
rossrangle <ross.kelly@rangle.io>
2020-04-20 15:31:35 -0400

    Images resized.

--- src/App.js
+++ src/App.js
@@ -25,32 +25,90 @@ import squirrel3 from "./assets/images/squirrel/squirrel3-min.png";
 
 import "./App.css";
 
+const hedgehogScene = {
+  icon: { src: hedgehogIcon, alt: "Hedgehog icon" },
+  textImage: { src: hedgehogText, alt: "Hedgehog" },
+  image1: { src: hedgehog1, alt: "Picture of hedgehog" },
+  image2: { src: hedgehog2, alt: "Picture of hedgehog" },
+  image3: { src: hedgehog3, alt: "Picture of hedgehog" },
+};
+
+const raccoonScene = {
+  icon: { src: raccoonIcon, alt: "raccoon icon" },
+  textImage: { src: raccoonText, alt: "raccoon" },
+  image1: { src: raccoon1, alt: "Picture of raccoon" },
+  image2: { src: raccoon2, alt: "Picture of raccoon" },
+  image3: { src: raccoon3, alt: "Picture of raccoon" },
+};
+
+const squirrelScene = {
+  icon: { src: squirrelIcon, alt: "squirrel icon" },
+  textImage: { src: squirrelText, alt: "squirrel" },
+  image1: { src: squirrel1, alt: "Picture of squirrel" },
+  image2: { src: squirrel2, alt: "Picture of squirrel" },
+  image3: { src: squirrel3, alt: "Picture of squirrel" },
+};
+
 function App() {
-  const scene = {
-    icon: { src: hedgehogIcon, alt: "Hedgehog icon" },
-    textImage: { src: hedgehogText, alt: "Hedgehog" },
-    image1: { src: hedgehog1, alt: "Picture of hedgehog" },
-    image2: { src: hedgehog2, alt: "Picture of hedgehog" },
-    image3: { src: hedgehog3, alt: "Picture of hedgehog" },
-  };
+  const scene = squirrelScene;
 
   /* Framer motion: orchestration */
   return (
-    <div className="hero">
-      <IconHolder icon={scene.icon} textImage={scene.textImage} />
-      <ImageHolder
-        img={scene.image1}
-        className="animal-image animal-image__one"
-      />
-      <ImageHolder
-        img={scene.image2}
-        className="animal-image animal-image__two"
-      />
-      <ImageHolder
-        img={scene.image3}
-        className="animal-image animal-image__three"
-      />
-    </div>
+    <>
+      <div className="hero">
+        <IconHolder icon={scene.icon} textImage={scene.textImage} />
+        <ImageHolder
+          img={scene.image1}
+          className="animal-image animal-image__one"
+        />
+        <ImageHolder
+          img={scene.image2}
+          className="animal-image animal-image__two"
+        />
+        <ImageHolder
+          img={scene.image3}
+          className="animal-image animal-image__three"
+        />
+      </div>
+
+      <div className="hero">
+        <IconHolder
+          icon={raccoonScene.icon}
+          textImage={raccoonScene.textImage}
+        />
+        <ImageHolder
+          img={raccoonScene.image1}
+          className="animal-image animal-image__one"
+        />
+        <ImageHolder
+          img={raccoonScene.image2}
+          className="animal-image animal-image__two"
+        />
+        <ImageHolder
+          img={raccoonScene.image3}
+          className="animal-image animal-image__three"
+        />
+      </div>
+
+      <div className="hero">
+        <IconHolder
+          icon={hedgehogScene.icon}
+          textImage={hedgehogScene.textImage}
+        />
+        <ImageHolder
+          img={hedgehogScene.image1}
+          className="animal-image animal-image__one"
+        />
+        <ImageHolder
+          img={hedgehogScene.image2}
+          className="animal-image animal-image__two"
+        />
+        <ImageHolder
+          img={hedgehogScene.image3}
+          className="animal-image animal-image__three"
+        />
+      </div>
+    </>
   );
 }
 
