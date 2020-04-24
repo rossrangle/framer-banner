3ca7583fb2aee26190065afc2fe7e8d03ce03a95
rossrangle <ross.kelly@rangle.io>
2020-04-24 11:51:43 -0400

    variants

--- src/IconHolder.js
+++ src/IconHolder.js
@@ -2,6 +2,20 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const ANIMATION_DURATION_S = 0.8;
+
+const getVariants = (direction) => ({
+  initial: {
+    y: direction === "top" ? "-100%" : "100%",
+    opacity: 0,
+    transition: { duration: ANIMATION_DURATION_S, ease: "easeInOut" },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: ANIMATION_DURATION_S, ease: "easeInOut" },
+  },
+});
+
 function IconHolder({ icon, text }) {
   return (
     <div className="icon-holder">
@@ -9,20 +23,20 @@ function IconHolder({ icon, text }) {
         <motion.div
           className="icon-holder__icon"
           key={text + "icon"}
-          initial={{ y: "-100%", opacity: 0 }}
-          exit={{ y: "-100%", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: ANIMATION_DURATION_S, ease: "easeInOut" }}
+          variants={getVariants("top")}
+          initial={"initial"}
+          exit={"initial"}
+          animate={"animate"}
         >
           <img src={icon.src} alt={icon.alt} />
         </motion.div>
         <motion.h1
           className="icon-holder__text"
           key={text}
-          initial={{ y: "100%", opacity: 0 }}
-          exit={{ y: "100%", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: ANIMATION_DURATION_S, ease: "easeInOut" }}
+          variants={getVariants("bottom")}
+          initial={"initial"}
+          exit={"initial"}
+          animate={"animate"}
         >
           {text}
         </motion.h1>
