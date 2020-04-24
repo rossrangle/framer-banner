7dafb74721a3774741f846770adf018e386fb60c
rossrangle <ross.kelly@rangle.io>
2020-04-24 11:19:32 -0400

    rotate, fade

--- src/ImageHolder.js
+++ src/ImageHolder.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const TRANSITION_TIME_OPACITY_S = 1;
+const TRANSITION_TIME_ROTATE_S = 2;
 
 function ImageHolder({ img, className }) {
   return (
-    <div className={className ? className : undefined}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, rotate: [-2, 0, 2] }}
+      transition={{
+        duration: TRANSITION_TIME_OPACITY_S,
+        rotate: { yoyo: Infinity, duration: TRANSITION_TIME_ROTATE_S },
+      }}
+      className={className ? className : undefined}
+    >
       <img src={img.src} alt={img.alt} />
-    </div>
+    </motion.div>
   );
 }
 
