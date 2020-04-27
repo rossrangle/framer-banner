7090b82f114fc70b343de81eb4fea9e1a25e23f6
Ross Kelly <51920545+rossrangle@users.noreply.github.com>
2020-04-27 10:54:50 -0400

    Step 6 - Animate logo and header

--- src/IconHolder.js
+++ src/IconHolder.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
+const ANIMATION_DURATION_S = 0.8;
 function IconHolder({ icon, text }) {
   return (
     <div className="icon-holder">
-      <div className="icon-holder__icon">
-        <img src={icon.src} alt={icon.alt} />
-      </div>
-      <h1 className="icon-holder__text">{text}</h1>
+      <AnimatePresence>
+        <motion.div
+          className="icon-holder__icon"
+          key={text + "icon"}
+          initial={{ y: "-100%", opacity: 0 }}
+          exit={{ y: "-100%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: ANIMATION_DURATION_S, ease: "easeInOut" }}
+        >
+          <img src={icon.src} alt={icon.alt} />
+        </motion.div>
+        <motion.h1
+          className="icon-holder__text"
+          key={text}
+          initial={{ y: "100%", opacity: 0 }}
+          exit={{ y: "100%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: ANIMATION_DURATION_S, ease: "easeInOut" }}
+        >
+          {text}
+        </motion.h1>
+      </AnimatePresence>
     </div>
   );
 }
