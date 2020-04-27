82e6fad5823ef89678bb200f3e03a8b944206b4a
Ross Kelly <51920545+rossrangle@users.noreply.github.com>
2020-04-27 10:48:36 -0400

    Step 4 - useCycle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCycle } from "framer-motion";
 
 import ImageHolder from "./ImageHolder";
 import IconHolder from "./IconHolder";
@@ -6,9 +7,20 @@ import IconHolder from "./IconHolder";
 import { hedgehogScene, raccoonScene, squirrelScene } from "./scenes";
 
 import "./App.css";
+const SLIDE_CHANGE_TIME_MS = 5000;
 
 function App() {
-  const currentScene = hedgehogScene;
+  const [currentScene, setCurrentScene] = useCycle(
+    hedgehogScene,
+    raccoonScene,
+    squirrelScene
+  );
+
+  useEffect(() => {
+    const timeOut = setTimeout(setCurrentScene, SLIDE_CHANGE_TIME_MS);
+    return () => clearTimeout(timeOut);
+  }, [currentScene, setCurrentScene]);
+
   return (
     <div className="hero">
       <IconHolder icon={currentScene.icon} text={currentScene.text} />
